704a19a4d632c643eca632cf0aabf9ec1dab43b0
dcheng093 <chengpeter37093@gmail.com>
2026-08-24 17:53:57 +0800

    stuff

--- test.py
+++ test.py
@@ -114,3 +114,9 @@ print(merge_and_sort_desc([1, 3, 5], [2, 4, 6]))  # [6, 5, 4, 3, 2, 1]
 print(merge_and_sort_desc([10, 2], [3, 7, 2]))    # [10, 7, 3, 2, 2]
 print(merge_and_sort_desc([], [1, 2, 3]))         # [3, 2, 1]
 print(merge_and_sort_desc([], []))                # []
+print("\nvalid_anagram\n")
+print(valid_anagram("racecar", "carrace"))        # True
+print(valid_anagram("jar", "jam"))                # False
+print(valid_anagram("listen", "silent"))          # True
+print(valid_anagram("aabbcc", "abcabc"))          # True
+print(valid_anagram("abc", "ab"))                 # False
